acb4092c4e49361210b930ad5e7b33472f07b576
Aaron <mohammad.chegini@hotmail.com>
2020-10-08 12:23:35 +0330

    Refactored User.js

--- src/Components/Repos/Repos.js
+++ src/Components/Repos/Repos.js
@@ -2,11 +2,12 @@ import React, { Fragment } from "react";
 import RepoItem from "./RepoItem";
 
 const Repos = ({ repos }) => {
+  let i = 0;
   return (
     <Fragment>
       <div className="card">
         {repos.map((element) => (
-          <RepoItem repo={element} />
+          <RepoItem repo={element} key={i++} />
         ))}
       </div>
     </Fragment>
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import Repos from "./Repos/Repos";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,100 +7,105 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import Spinner from "./Spinner";
+import { Link } from "react-router-dom";
 
-export default class User extends Component {
-  hireableIcon = (
+let hireableIcon = (
     <FontAwesomeIcon icon={faCheckCircle} className="text-success" />
-  );
+  ),
   notHireable = (
     <FontAwesomeIcon icon={faTimesCircle} style={{ color: "red" }} />
   );
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
+
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+  }, [getUserRepos, getUser, match.params.login]);
+
+  const {
+    name,
+    hireable,
+    avatar_url,
+    login,
+    html_url,
+    location,
+    bio,
+    created_at,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+  } = user;
+
+  const userIcon = <FontAwesomeIcon icon={faUser} />;
+
+  if (loading) {
+    return <Spinner />;
   }
-  render() {
-    const {
-      name,
-      hireable,
-      avatar_url,
-      login,
-      html_url,
-      location,
-      bio,
-      created_at,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-    } = this.props.user;
 
-    const userIcon = <FontAwesomeIcon icon={faUser} />;
-
-    if (this.props.loading) {
-      return <Spinner />;
-    }
-
-    return (
-      <Fragment>
-        <div className="btn btn-dark">Back to search</div>
-        Hireable: {hireable ? this.hireableIcon : this.notHireable}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              className="round-img"
-              alt=""
-              style={{ maxWidth: "250px" }}
-            />
-            <h1>{name}</h1>
-            {location && <h3>Location: {location}</h3>}
-            <br />
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <br />
-            {login && (
-              <Fragment>
-                <h3>Username</h3>
-                <p>{login}</p>
-              </Fragment>
-            )}
-            <br />
-            {created_at && (
-              <Fragment>
-                <h3>Created at:</h3>
-                <p>{new Date(created_at).toDateString()}</p>
-              </Fragment>
-            )}
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={html_url}
-              className="btn btn-dark"
-            >
-              Visit this user on Github!
-            </a>
-          </div>
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-dark">
+        Back to search
+      </Link>
+      Hireable: {hireable ? hireableIcon : notHireable}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt=""
+            style={{ maxWidth: "250px" }}
+          />
+          <h1>{name}</h1>
+          {location && <h3>Location: {location}</h3>}
+          <br />
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">
-            {userIcon} Followers: {followers}
-          </div>
-          <div className="badge badge-success">
-            {userIcon} Following: {following}
-          </div>
-          <div className="badge badge-light">Public Gists: {public_gists}</div>
-          <div className="badge badge-dark">Repositories: {public_repos}</div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <br />
+          {login && (
+            <Fragment>
+              <h3>Username</h3>
+              <p>{login}</p>
+            </Fragment>
+          )}
+          <br />
+          {created_at && (
+            <Fragment>
+              <h3>Created at:</h3>
+              <p>{new Date(created_at).toDateString()}</p>
+            </Fragment>
+          )}
+          <br />
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={html_url}
+            className="btn btn-dark"
+          >
+            Visit this user on Github!
+          </a>
         </div>
-        <Repos repos={this.props.repos} />
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">
+          {userIcon} Followers: {followers}
+        </div>
+        <div className="badge badge-success">
+          {userIcon} Following: {following}
+        </div>
+        <div className="badge badge-light">Public Gists: {public_gists}</div>
+        <div className="badge badge-dark">Repositories: {public_repos}</div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+export default User;
